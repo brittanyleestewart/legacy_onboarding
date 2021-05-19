bac1afc12bae7576b0ed590d5a6550094e21d660
Brittany Stewart <brittanyleebird@gmail.com>
2021-05-19 17:14:02 -0500

    This is my first commit

---
+++ codecept.conf.js
@@ -0,0 +1,38 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: 'tests/*.test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'http://localhost',
+      browser: 'chrome'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'legacy',
+  plugins: {
+    wdio: {
+      enabled: true,
+      services: ['selenium-standalone']
+    },
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
---
+++ tests/obituaries.test.js
@@ -0,0 +1,5 @@
+Feature('obituaries').tag('@onboarding');
+
+Scenario('test something', ({ I }) => {
+    I.amOnPage('https://github.com'); // opens github
+});
